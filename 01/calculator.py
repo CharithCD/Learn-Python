d92d75e577d2b53f152328f91fc595adc90f8856
CharithCD <damruwanweerasinghe1@gmail.com>
2024-03-07 20:20:49 +0530

    number formatting

--- 01/calculator.py
+++ 01/calculator.py
@@ -1,14 +1,19 @@
-# int
-x = int(input("Enter a number: "));
-y = int(input("Enter another number: "));
-print(x + y);
+# # int
+# x = int(input("Enter a number: "));
+# y = int(input("Enter another number: "));
+# print(x + y);
 
-# float
-x = float(input("Enter a number: "));
-y = float(input("Enter another number: "));
-print(int(x + y));
-print(round(x + y));
-print(round((x + y), 9));
-print(round((x + y), 2));
-print(round((x + y), 1));
+# # float
+# x = float(input("Enter a number: "));
+# y = float(input("Enter another number: "));
+# print(int(x + y));
+# print(round(x + y));
+# print(round((x + y), 9));
+# print(round((x + y), 2));
+# print(round((x + y), 1));
 
+
+#Number formatting with f strings
+a = 1000000;
+print(f"{a:e}"); # scientific notation
+print(f"{a:,}"); # add comma to the number
