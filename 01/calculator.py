179e4051fab8ece41f888077727c1eb79f04073f
CharithCD <damruwanweerasinghe1@gmail.com>
2024-03-07 20:07:29 +0530

    learning Float

--- 01/calculator.py
+++ 01/calculator.py
@@ -1,5 +1,14 @@
+# int
 x = int(input("Enter a number: "));
 y = int(input("Enter another number: "));
-
-# kcek
 print(x + y);
+
+# float
+x = float(input("Enter a number: "));
+y = float(input("Enter another number: "));
+print(int(x + y));
+print(round(x + y));
+print(round((x + y), 9));
+print(round((x + y), 2));
+print(round((x + y), 1));
+
